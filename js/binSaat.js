bb8ea535c500ad4c3a171485a26662a870d3d58b
Osman Nuri <onokumus@gmail.com>
2013-04-22 19:42:40 +0300

    convert to jQuery

--- js/binSaat.js
+++ js/binSaat.js
@@ -1,59 +1,72 @@
 var renkVer = function (n, k, c) {
-    dojo.removeClass(k + "1", c);
-    dojo.removeClass(k + "2", c);
-    dojo.removeClass(k + "4", c);
-    dojo.removeClass(k + "8", c);
+
+    $(k+'1').removeClass(c);
+    $(k+'2').removeClass(c);
+    $(k+'4').removeClass(c);
+    $(k+'8').removeClass(c);
+
     if (n == 9) {
-        dojo.addClass(k + "1", c);
-        dojo.addClass(k + "8", c);
+        $(k+'1').addClass(c);
+        $(k+'8').addClass(c);
     } else if (n == 8) {
-        dojo.addClass(k + "8", c);
+        $(k+'8').addClass(c);
     } else if (n == 7) {
-        dojo.addClass(k + "1", c);
-        dojo.addClass(k + "2", c);
-        dojo.addClass(k + "4", c);
+        $(k+'1').addClass(c);
+        $(k+'2').addClass(c);
+        $(k+'4').addClass(c);
     } else if (n == 6) {
-        dojo.addClass(k + "2", c);
-        dojo.addClass(k + "4", c);
+        $(k+'2').addClass(c);
+        $(k+'4').addClass(c);
     } else if (n == 5) {
-        dojo.addClass(k + "1", c);
-        dojo.addClass(k + "4", c);
+        $(k+'1').addClass(c);
+        $(k+'4').addClass(c);
     } else if (n == 4) {
-        dojo.addClass(k + "4", c);
+        $(k+'4').addClass(c);
     } else if (n == 3) {
-        dojo.addClass(k + "1", c);
-        dojo.addClass(k + "2", c);
-    } else if (n == 2) {
-        dojo.addClass(k + "2", c);
-    } else if (n == 1) {
-        dojo.addClass(k + "1", c);
-    } else {
-        dojo.removeClass(k + "1", c);
-        dojo.removeClass(k + "2", c);
-        dojo.removeClass(k + "4", c);
-        dojo.removeClass(k + "8", c);
-    }
+       $(k+'1').addClass(c);
+       $(k+'2').addClass(c);
+   } else if (n == 2) {
+    $(k+'2').addClass(c);
+} else if (n == 1) {
+    $(k+'1').addClass(c);
+} 
+
+else {
+    $(k+'1').removeClass(c);
+    $(k+'2').removeClass(c);
+    $(k+'4').removeClass(c);
+    $(k+'8').removeClass(c);
 }
+}
+
+
 function setSaat() {
     var now = new Date();
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
+
+
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
     var saatK = hour % 10;
-    renkVer(saatK, "saatK", "red");
+    renkVer(saatK, "#saatK", "red");
     var saatB = (hour - saatK) / 10;
-    renkVer(saatB, "saatB", "red");
+    renkVer(saatB, "#saatB", "red");
     var dakikaK = minute % 10;
-    renkVer(dakikaK, "dakikaK", "red");
+    renkVer(dakikaK, "#dakikaK", "red");
     var dakikaB = (minute - dakikaK) / 10;
-    renkVer(dakikaB, "dakikaB", "red");
+    renkVer(dakikaB, "#dakikaB", "red");
     var saniyeK = second % 10;
-    renkVer(saniyeK, "saniyeK", "red");
+    renkVer(saniyeK, "#saniyeK", "blue");
     var saniyeB = (second - saniyeK) / 10;
-    renkVer(saniyeB, "saniyeB", "red");
-    dojo.attr("saat", "innerHTML", sifirEkle(hour) + ":" + sifirEkle(minute) + ":" + sifirEkle(second));
-    window.setTimeout("setSaat()", 1000);
+    renkVer(saniyeB, "#saniyeB", "blue");
+
+    $('#saat').html(sifirEkle(hour) + ":" + sifirEkle(minute) + ":" + sifirEkle(second));            
 }
+
 function sifirEkle(number) {
     if (number < 10) {
         return "0" + number.toString();
@@ -61,4 +74,5 @@ function sifirEkle(number) {
         return number.toString();
     }
 }
-dojo.ready(setSaat);
+
+window.setInterval(function(){setSaat();}, 1000);
